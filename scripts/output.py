5cdcd1a9fa4f441839229cea558de22962d8f629
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-01-06 14:12:36 +0000

    Update output.py

--- scripts/output.py
+++ scripts/output.py
@@ -5,4 +5,4 @@ opensdg_output = sdg.outputs.OutputOpenSdg(
     schema=schema,
     output_folder='_site',
     translations=translations,
-    reporting_status_extra_fields=['an_extra_field', 'another_extra_field'])
+    reporting_status_extra_fields=['un_designated_tier'])
