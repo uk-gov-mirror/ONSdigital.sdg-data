f243eef5fe5a6c72d1a0c17ee97d21051d44198b
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-01-13 13:51:23 +0000

    Update output.py

--- scripts/output.py
+++ scripts/output.py
@@ -17,6 +17,7 @@ schema_path = os.path.join('_prose.yml')
 schema = sdg.schemas.SchemaInputOpenSdg(schema_path=schema_path)
 
 # Use SDG Translations for translations
+all_translations = [sdg.translations.TranslationInputSdgTranslations(source='https://github.com/open-sdg/translations-un-sdg.git', repo='1.0.0-rc1')]
 
 # Indicate any extra fields for the reporting stats, if needed.
 reporting_status_extra_fields = ['un_designated_tier']
@@ -25,7 +26,7 @@ opensdg_output = sdg.outputs.OutputOpenSdg(
     inputs=inputs,
     schema=schema,
     output_folder='_site',
-    translations=None,
+    translations=all_translations,
     reporting_status_extra_fields=reporting_status_extra_fields)
 
 # Validate the indicators.
