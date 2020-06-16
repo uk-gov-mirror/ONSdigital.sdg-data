3ef2f7148c7c0e854ff1fb4435479f3bc2df47dc
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-06-16 13:22:30 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,7 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(meta):
+    meta['indicator_number'] = meta['indicator']
+    return meta
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
