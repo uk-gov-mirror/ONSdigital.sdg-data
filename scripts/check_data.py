d958864f6bf3997d6a03c42bf45fecb2916053df
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-06-16 13:21:50 +0100

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,12 @@
 from sdg.open_sdg import open_sdg_check
 
+# alter meta
+def alter_meta(meta):
+    meta['indicator_number'] = meta['indicator']
+    return meta
+
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_meta=alter_meta)
 
 # If everything was valid, perform the build.
 if not validation_successful:
